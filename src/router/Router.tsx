21972ab4b3ae5e566b3bb84153489621690c1d56
krishnabakshi2455 <krishnabakshi2455@gmail.com>
2024-09-17 20:41:36 +0530

    first commit

---
+++ src/router/Router.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from 'react-router-dom'
+import Homepage from '../pages/Homepage'
+
+const Router = () => {
+  return (
+    <>
+    <Routes>
+        <Route path="/" element={<Homepage/>} />
+    </Routes>
+    </>
+  )
+}
+
+export default Router
